4d456aee2c400b77665a59c28f5f141a62e89954
DanielaIH <4427@holbertonschool.com>
2022-07-25 15:08:20 -0500

    Style corrections

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,11 +6,11 @@
  * Return: 1 if Node is a leaf, 0 otherwise.
  */
 
- int binary_tree_is_leaf(const binary_tree_t *node)
- {
-    if (node == NULL)
-        return (0);
-    if (node->right == NULL && node->left == NULL)
-        return (1);
-    return (0);
- }
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->right == NULL && node->left == NULL)
+		return (1);
+	return (0);
+}
