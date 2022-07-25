3a720d293be5a14422c8dccb986350d3fd61b74c
DanielaIH <4427@holbertonschool.com>
2022-07-25 14:53:17 -0500

    ADD 3-binary_tree_delete.c that deletes a entire tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -12,11 +12,5 @@
         return;
     binary_tree_delete(tree->left);
     binary_tree_delete(tree->right);
-/*    if (tree->left)
-        free(tree->left);
-    if (tree->right)
-        free(tree->right);
-    if (tree->left == NULL && tree->right == NULL) */
-    printf("Node to delete %d\n", tree->n); /* BORRAR */
     free(tree);
  }
