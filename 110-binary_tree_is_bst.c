85c900d5f992071d602e1ef1dd23e839c3d9bf2f
sevajal <sevajal@yahoo.com>
2022-07-27 12:36:38 -0500

    ADD 110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -2,14 +2,14 @@
 #include <limits.h>
 
 /**
- * checkBTS - checks if a binary tree is a valid BST.
+ * checkBST - checks if a binary tree is a valid BST.
  * @node: pointer to the node of the tree to check.
  * @min: INT_MIN.
  * @max: INT_MAX.
- * Return: 1 if it is BTS, 0 otherwise.
+ * Return: 1 if it is BST, 0 otherwise.
  */
 
-int checkBTS(binary_tree_t *node, int min, int max)
+int checkBST(binary_tree_t *node, int min, int max)
 {
 	if (!node)
 		return (1);
@@ -17,14 +17,14 @@ int checkBTS(binary_tree_t *node, int min, int max)
 	if (node->n < min || node->n > max)
 		return (0);
 
-	return (checkBTS(node->left, min, node->n - 1) &&
-	checkBTS(node->right, node->n + 1, max));
+	return (checkBST(node->left, min, node->n - 1) &&
+	checkBST(node->right, node->n + 1, max));
 }
 
 /**
  * binary_tree_is_bst - checks if a binary tree is a valid BST.
  * @tree: pointer to the root node of the tree to check.
- * Return: 1 if it is BTS, 0 otherwise.
+ * Return: 1 if it is BST, 0 otherwise.
  */
 
 int binary_tree_is_bst(const binary_tree_t *tree)
@@ -32,5 +32,5 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	return (checkBTS((binary_tree_t *)tree, INT_MIN, INT_MAX));
+	return (checkBST((binary_tree_t *)tree, INT_MIN, INT_MAX));
 }
