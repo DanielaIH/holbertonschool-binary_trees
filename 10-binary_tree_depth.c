69ca82c7d3c78f53334e6dea9349ec4a50900800
sevajal <sevajal@yahoo.com>
2022-07-27 08:51:44 -0500

    UPDATE to fix the null tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,6 +11,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	const binary_tree_t *current = tree;
 	size_t depth = 0;
 
+	if (!tree)
+		return (0);
+
 	while (current->parent)
 	{
 		current = current->parent;
